0dc8c1a44336de71b66540ed76293dd5091d0961
charrismagparoc <magparoc.charris10@gmail.com>
2026-05-25 17:42:09 +0800

    updated

--- email_config.py
+++
@@ -1,3 +0,0 @@
-MAIL_USERNAME = "idrms.kauswagan@gmail.com"
-MAIL_PASSWORD = "lptdgcltnautwusx"
-MAIL_FROM = "idrms.kauswagan@gmail.com"
--- fix_email.py
+++
@@ -1,7 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    conn.execute(text("ALTER TABLE residents ADD COLUMN email VARCHAR DEFAULT ''"))
-    conn.commit()
-    print("Done!")
